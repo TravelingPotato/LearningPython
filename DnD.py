e44aea849f58a2883ff66e6c2202ccfd29ac8e28
Tristan Anderson <systeatristan@gmail.com>
2018-07-15 15:50:23 -0500

    Fixed

--- DnD.py
+++ DnD.py
@@ -2,7 +2,6 @@ import random
 
 def roll(roll_num,sides):
     total = 0 
-    while roll_num:
+    for roll in range(roll_num):
         total += random.randrange(1,sides+1)
-        roll_num -= 1
     return total
