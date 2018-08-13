8ccf313bf7d6b25e1a65038f67c72bbbb5f83c6c
TravelingPotato <ande5460@gmail.com>
2018-08-12 21:02:25 -0500

    8.12.2018

---
+++ FirstNeuralNetwork.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from scipy.io import loadmat
+
+data = loadmat('c:\\Users\\ande5\\LearningPython\\data\\ex3data1.mat')
+
+print(data)
